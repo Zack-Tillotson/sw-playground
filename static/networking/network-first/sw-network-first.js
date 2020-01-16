3142347eca3f4804423711a61f79819c70eee9ec
Zack Tillotson <ZTillotson@aetna.com>
2020-01-16 14:58:25 -0700

    Sharing the static kitten image between various networking examples. Adding the network-first sw file

---
+++ static/networking/network-first/sw-network-first.js
@@ -0,0 +1,38 @@
+const cacheVersion = 'sw-network-first';
+
+const filesToPreload = [
+  'cashKitten.jpg',
+]
+
+self.addEventListener('install', event => {
+  console.log(cacheVersion, 'install');
+  event.waitUntil(
+    caches.open(cacheVersion)
+      .then(cache => cache.addAll(filesToPreload))
+  )
+});
+
+self.addEventListener('fetch', event => {
+  console.log(swVersion, 'fetch');
+
+  if(!event.request.url.endsWith('.jpg')) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .catch(error => {
+        console.log(swVersion, 'fetch', 'unable to fetch, looking in cache');
+
+        return caches
+          .open(cacheVersion)
+          .then(cache => cache.match(event.request))
+          .then(resp => {
+            if(resp) {
+              console.log(swVersion, 'fetch', 'found response in cache, returning that');
+              return resp;
+            }
+
+            throw error;
+          })
+        })
+  )
+});
