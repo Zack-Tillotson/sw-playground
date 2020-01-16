01139803b1b7cabb4652483435f215fdb5f0d058
Zack Tillotson <ZTillotson@aetna.com>
2020-01-16 15:11:55 -0700

    Fixes sw-network-first.js reference to old var name

--- static/networking/network-first/sw-network-first.js
+++ static/networking/network-first/sw-network-first.js
@@ -13,21 +13,21 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', event => {
-  console.log(swVersion, 'fetch');
+  console.log(cacheVersion, 'fetch');
 
   if(!event.request.url.endsWith('.jpg')) return;
 
   event.respondWith(
     fetch(event.request)
       .catch(error => {
-        console.log(swVersion, 'fetch', 'unable to fetch, looking in cache');
+        console.log(cacheVersion, 'fetch', 'unable to fetch, looking in cache');
 
         return caches
           .open(cacheVersion)
           .then(cache => cache.match(event.request))
           .then(resp => {
             if(resp) {
-              console.log(swVersion, 'fetch', 'found response in cache, returning that');
+              console.log(cacheVersion, 'fetch', 'found response in cache, returning that');
               return resp;
             }
 
